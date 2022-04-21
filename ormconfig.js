b96813aff49a7fed70c162a75f98efc7c0852bd5
Varun <varunkverma1996@gmail.com>
2022-04-22 00:39:31 +0530

    nestjs prod db config

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-const dbCOnfig = {
+const dbConfig = {
   synchronize: false,
   migrations: ['migrations/*.js'],
   cli: {
@@ -8,14 +8,14 @@ const dbCOnfig = {
 
 switch (process.env.NODE_ENV) {
   case 'development':
-    Object.assign(dbCOnfig, {
+    Object.assign(dbConfig, {
       type: 'sqlite',
       database: 'db.sqlite',
       entities: ['**/*.entity.js'],
     });
     break;
   case 'test':
-    Object.assign(dbCOnfig, {
+    Object.assign(dbConfig, {
       type: 'sqlite',
       database: 'test.sqlite',
       entities: ['**/*.entity.ts'],
@@ -23,9 +23,18 @@ switch (process.env.NODE_ENV) {
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
     break;
   default:
     throw new Error('unknown environment');
 }
 
-module.exports = dbCOnfig;
+module.exports = dbConfig;
